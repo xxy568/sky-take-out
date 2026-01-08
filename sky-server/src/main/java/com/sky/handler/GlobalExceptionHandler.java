265640ff07b482ae5f4b38efa8a9176a15cc8d35
XXY <254536412@qq.com>
2026-01-08 10:43:35 +0800

    新增员工

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -23,5 +27,17 @@ public class GlobalExceptionHandler {
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
     }
-
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException e){
+        //报错信息： Duplicate entry 'huachengyu' for key 'employee.idx_username'
+        String message=e.getMessage();
+        if(message.contains("Duplicate entry")){
+          String[] split=message.split(" ");
+          String msg=split[2]+ MessageConstant.ALREADY_EXISTS;
+          return Result.error(msg);
+        }
+        else {
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
+    }
 }
